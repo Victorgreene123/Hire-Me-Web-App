5aa2d6f497c3daa1dde1edc91e5c546b7fc530b4
Vickygreene123 <okereaforfavour867@gmail.com>
2024-07-25 08:17:53 +0000

    rrr

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -25,16 +25,16 @@ const Navbar = (props) => {
     return (
       <nav
         className={`flex w-full h-[65px] lg:px-5 p-3 ${props.isHomePage ? 'bg-transparent': 'bg-white'} text-[18px] items-center ${
-          isFixed ? 'fixed top-0 left-0 z-50 shadow-lg bg-white' : ''
+          isFixed ? 'fixed top-0 left-0 z-50 shadow-lg bg-[#340352]' : ''
         }`}
       >
-        <i className={`fas fa-bars ${ isFixed ? 'text-gray-500' : 'text-white'} lg:hidden`}></i>
+        <i className={`fas fa-bars ${ isFixed ? 'text-white' : 'text-white'} lg:hidden`}></i>
         <img src={logo} alt="Logo" className="w-24 h-auto" />
         <div className="absolute flex items-center lg:hidden right-4">
           <Link to="/find-professionals" className="mr-3 text-[1rem]">
             <span className="text-outline text-stroke-purple open-sans-normal">Log in</span>
           </Link>
-          <i className={`fas fa-search ${ isFixed ? 'text-gray-500' : 'text-white'}`}></i>
+          <i className={`fas fa-search ${ isFixed ? 'text-white' : 'text-white'}`}></i>
         </div>
         <div className="hidden lg:flex items-center flex-grow">
           <Link to="/find-professionals" className="ml-10 text-[1rem]">
@@ -47,7 +47,7 @@ const Navbar = (props) => {
               className="input-field w-full pl-4 p-1 pr-12 border border-gray-300 rounded-full text-white text-sm"
             />
             <span className="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
-              <i className="fas fa-search text-gray-500"></i> {/* Font Awesome icon */}
+              <i className="fas fa-search text-white"></i> {/* Font Awesome icon */}
             </span>
           </div>
           <Link to="/find-professionals" className="mr-8 text-[1rem]">
--- frontend/src/pages/homepage.js
+++ frontend/src/pages/homepage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import '../customstyles.css'
 import heroimage1 from '../Assets/heroimage_1.jpg'
@@ -6,11 +6,29 @@ import heroimage2 from '../Assets/heroimage_2.jpg'
 import heroimage3 from '../Assets/heroimage_3.jpg'
 import heroimage4 from '../Assets/heroimage_4.jpg'
 import heroimage5 from '../Assets/heroimage_5.jpg'
+import pageui from '../Assets/pageui.png'
 
 const Home =() =>{
+    const [isFixed, setIsFixed] = useState(false);
+  
+    useEffect(() => {
+      const handleScroll = () => {
+        if (window.scrollY > 50) {
+          setIsFixed(true);
+        } else {
+          setIsFixed(false);
+        }
+      };
+  
+      window.addEventListener('scroll', handleScroll);
+  
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+      };
+    }, []);
     return(
         <>
-        <div className="relative hero lg:h-[90vh] h-[62vh] w-full ">
+        <div className={`relative hero ${ isFixed ?  'lg:h-[80vh] h-[42vh]' : 'lg:h-[90vh] h-[62vh]'}  w-full `}>
         <Navbar isHomePage={true}/>
        
         <div className="   w-[100%] mx-auto lg:flex block gap-10 pl-8 pr-8 lg:pl-5 lg:pr-0 lg:mt-12 mt-20">
@@ -30,8 +48,18 @@ const Home =() =>{
         </div>
         {/* <div className=" h-full bg-[#340352] opacity-20 absolute top-0 left-0 w-full"></div> */}
        </div>
-      
+       <div className="lg:flex block w-full p-3 lg:px-5 h-[40vh] lg:h-[60vh] gap-5 lg:mt-10 mt-3">
+        <div className="border-[#505168] rounded-md bg-[#27233A] h-full lg:w-2/5 mb-5 p-2 items-center">
+        <img className="w-full" src={pageui}></img>
+        </div>
+       
+        <div className=" h-full lg:w-3/4 ">
+        <h1 className="tracking-tighter lg:text-5xl font-semibold text-4xl text-left lg:text-left">Up your work game, <span className="text-outline-lg text-outline text-stroke-purple text-transparent">it’s easy</span></h1>
 
+</div>
+        
+       </div>
+     
         </>
     )
 }
