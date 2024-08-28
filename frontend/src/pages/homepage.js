f492c91c82401cb8d3a506f09aaf1c94e68fbe93
Vickygreene123 <okereaforfavour867@gmail.com>
2024-08-28 14:46:21 +0100

    Making Changes

--- frontend/src/pages/homepage.js
+++ frontend/src/pages/homepage.js
@@ -8,9 +8,11 @@ import heroimage2 from '../Assets/heroimage_2.jpg'
 import heroimage3 from '../Assets/heroimage_3.jpg'
 import heroimage4 from '../Assets/heroimage_4.jpg'
 import heroimage5 from '../Assets/heroimage_5.jpg'
+import picture1 from '../Assets/agreement-concept-illustration.png'
+import picture2 from '../Assets/searchpeople.png'
 import pageui from '../Assets/pageui.png'
 import flyer from '../Assets/HIREME FLYER_010334.jpg'
-import { FaDollarSign, FaEdit, FaHandshake, FaHandshakeAltSlash, FaLocationArrow, FaMapMarked, FaMapMarkedAlt, FaMapMarker, FaMapMarkerAlt, FaMapPin, FaMedal, FaMoneyBill, FaMoneyBillWave, FaSearchLocation } from 'react-icons/fa'; // Font Awesome Medal icon
+import { FaClock, FaDollarSign, FaEdit, FaHandshake, FaHandshakeAltSlash, FaLocationArrow, FaMapMarked, FaMapMarkedAlt, FaMapMarker, FaMapMarkerAlt, FaMapPin, FaMedal, FaMoneyBill, FaMoneyBillWave, FaPeopleCarry, FaSearchLocation, FaUserFriends, FaUserPlus, FaUsers } from 'react-icons/fa'; // Font Awesome Medal icon
 import { MdStar } from 'react-icons/md';
 const Home =() =>{
     const [isFixed, setIsFixed] = useState(false);
@@ -32,30 +34,33 @@ const Home =() =>{
     }, []);
     return(
         <>
-        <div className={`relative hero ${ isFixed ?  'lg:h-[80vh] h-[42vh]' : 'lg:h-[90vh] h-[62vh]'}  w-full `}>
+        <div className={`relative bg-[#340352] ${ isFixed ?  'lg:h-[80vh] h-auto' : 'lg:h-[90vh] h-auto'}  w-full `}>
+
         <Navbar isHomePage={true}/>
        
-        <div className="   w-[100%] mx-auto lg:flex block gap-10 pl-5 pr-2 lg:pl-5 lg:pr-0 lg:mt-12 mt-20">
-            <div className="lg:w-1/2 lg:px-2  mx-auto lg:mx-0 w-full">
-                <h1 className="text-white lg:text-6xl font-semibold text-4xl text-left lg:text-left "> Your Trusted Source for <span className="text-outline-lg text-outline text-stroke-purple "> Professional Services</span></h1>
-                <p className="text-[#B9A3A3] lg:w-1/2 lg:text-2xl mt-3">Find and Hire Reliable Professionals</p>
-                <button className="background-gradient py-5 px-12  rounded-full  text-white text-1xl lg:text-2xl lg:mt-5 mt-12 ">
+        <div className="   w-[100%] mx-auto lg:flex block gap-10 pl-5 pr-2 lg:pl-5 lg:pr-0 lg:mt-12 ">
+            
+            <div className="relative lg:w-1/2 lg:px-2  mx-auto lg:mx-0 w-full lg:pb-0 pb-5">
+            <img className="w-[68%]  mx-auto h-full lg:hidden rounded-full" src={picture1} alt="" />
+     
+
+                <h1 className="text-white  lg:text-6xl font-semibold text-4xl text-left lg:text-left z-10 lg:mt-0"> Your Trusted Source for <span className="text-outline-lg text-outline text-stroke-purple "> Professional Services</span></h1>
+                <p className="text-white lg:w-1/2 lg:text-1xl mt-3">Find and Hire Reliable Professionals</p>
+                <button className="background-gradient py-5 px-20  rounded-full  text-white text-1xl lg:text-1xl lg:mt-10 mt-12 ">
             Get Started
           </button>
             </div>
-            <div className="grid w-2/5 mx-auto px-10 grid-cols-2 gap-5 justify-center  scrolling hidden lg:grid">
-            <img className="" src={heroimage1} alt="" />
-            <img className=""src={heroimage4} alt="" />
-            <img className="" src={heroimage3} alt="" />
-            <img className="" src={heroimage4} alt="" />
+            <div className=" lg:w-2/5 w-full mx-auto px-10  gap-5 justify-center  ">
+            <img className="w-full lg:block hidden" src={picture1} alt="" />
+            
             </div>
         </div>
         {/* <div className=" h-full bg-[#340352] opacity-20 absolute top-0 left-0 w-full"></div> */}
        </div>
       <div className="px-5">
        <div className="lg:flex  block w-full p-3 lg:px-10  h-auto  lg:h-[70vh] gap-5 lg:mt-10  ">
-        <div className="border-[#505168] bg-[#f5f8f5] rounded-md flex  justify-center h-full lg:w-1/2 mb-5 p-2 items-center">
-        <img className="w-full  " src={pageui}></img>
+        <div className="border-[#505168] bg-[#f5f8f5] rounded-md flex  justify-center h-full lg:w-1/2 mb-5 lg:p-2 items-center">
+        <img className="w-full  " src={picture2}></img>
         </div>
        
         <div className=" h-full lg:w-3/4 lg:pt-5 mx-auto w-full px-2">
@@ -65,7 +70,7 @@ const Home =() =>{
             <FaEdit  className="text-2xl font-thin"/>
             <span className="block">
                 <h4 className="lg:text-2xl  text-1xl font-semibold">No cost to join</h4>
-                <h4 className="text-sm">Register and view various talent</h4>
+                <h4 className="text-sm">Register and browse through a variety of talented professionals at no cost.</h4>
             </span>
 
         </h3>
@@ -75,7 +80,7 @@ const Home =() =>{
 
             <span className="block">
                 <h4 className="lg:text-2xl  text-1xl font-semibold">Work with the best</h4>
-                <h4 className="text-sm">Explore our list of professionals</h4>
+                <h4 className="text-sm">Explore our curated list of top professionals in your area.</h4>
             </span>
 
         </h3>
@@ -83,13 +88,13 @@ const Home =() =>{
         <h3 className="flex items-center mt-2 p-2 gap-4" >
             {/* <i className=" text-2xl "></i> */}
           
-          <FaMapMarkerAlt className="text-2xl font-thin" />
+          <FaUserPlus className="text-2xl font-thin" />
 
             
             
             <span className="block ">
-                <h4 className="lg:text-2xl  text-1xl font-semibold">Closest to you</h4>
-                <h4 className="text-sm">Register and view various talent</h4>
+                <h4 className="lg:text-2xl  text-1xl font-semibold">Sign Up for Free</h4>
+                <h4 className="text-sm">Start connecting with experts who can help you achieve your goals.</h4>
             </span>
 
         </h3>
@@ -103,34 +108,34 @@ const Home =() =>{
 
        <div className=" bg-[#f9f9f9] mx-auto block w-full  rounded-lg lg:flex  lg:pl-12   h-auto   gap-5 mt-5  ">
         <div className="border-[#505168]  lg:py-5 rounded-md  h-full lg:w-4/5 mb-5 p-2 items-center">
-        <div className="px-4">
-        <h1 className="tracking-tighter lg:text-6xl font-semibold text-[1.8rem] text-left lg:text-left leading-tighter">Why you should use hireme</h1>
+        <div className="px-3">
+        <h1 className="tracking-tighter lg:text-6xl font-semibold text-[2.2rem] text-left lg:text-left leading-tighter">Why you should use hireme</h1>
         <h3 className="flex items-center mt-2 p-2 gap-4" >
          
          <FaDollarSign className="text-2xl font-thin"/>
          <span className="block">
-             <h4 className="lg:text-3xl  text-1xl font-semibold">Increase your Earning</h4>
-             <h4 className="text-sm">Register and view various talent</h4>
-         </span>
-
-     </h3>
-
-     <h3 className="flex items-center mt-2 p-2 gap-4" >
-         
-         <FaHandshakeAltSlash className="text-2xl font-thin"/>
-         <span className="block">
-             <h4 className="lg:text-3xl  text-1xl font-semibold">Work at your own time</h4>
-             <h4 className="text-sm">Register and view various talent</h4>
-         </span>
-
-     </h3>
-
-     <h3 className="flex items-center mt-2 p-2 gap-4" >
-         
-         <FaDollarSign className="text-2xl font-thin"/>
-         <span className="block">
              <h4 className="lg:text-3xl  text-1xl font-normal">Increase your Earning</h4>
-             <h4 className="text-sm">Register and view various talent</h4>
+             <h4 className="text-sm">Find opportunities that allow you to earn more by working with the best clients.</h4>
+         </span>
+
+     </h3>
+
+     <h3 className="flex items-center mt-2 p-2 gap-4" >
+         
+         <FaClock className="text-2xl font-thin"/>
+         <span className="block">
+             <h4 className="lg:text-3xl  text-1xl font-normal">Work at your own time</h4>
+             <h4 className="text-sm">Enjoy the flexibility of working on your schedule, making it easier to balance work and life.</h4>
+         </span>
+
+     </h3>
+
+     <h3 className="flex items-center mt-2 p-2 gap-4" >
+         
+         <FaUsers className="text-2xl font-thin"/>
+         <span className="block">
+             <h4 className="lg:text-3xl  text-1xl font-normal">Expand Your Reach</h4>
+             <h4 className="text-sm"> Get connected with clients seeking exactly what you offer.</h4>
          </span>
 
      </h3>
